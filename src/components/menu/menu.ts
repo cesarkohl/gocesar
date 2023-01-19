8660f6ef951c89e888bf6f53d5f16899e2c91c85
Cesar Kohl <cesarkohl@gmail.com>
2023-01-19 10:08:43 -0300

    WIP

---
+++ src/components/menu/menu.ts
@@ -0,0 +1,10 @@
+import './menu.scss';
+export const menu = `
+<nav class="menu">
+    <a href="#" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="#">Hire Talent</a>
+    <a href="#">How it works</a>
+</nav>
+`;
